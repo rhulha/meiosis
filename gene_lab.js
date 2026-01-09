14cd9034fc2512e2435402323e2a6b1d0aedf08b
java4life <java4life@gmail.com>
2026-01-09 22:22:18 +0100

    Update GeneLab to adjust camera position and implement ball addition methods

--- gene_lab.js
+++ gene_lab.js
@@ -11,7 +11,7 @@ export class GeneLab {
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.container.appendChild(this.renderer.domElement);
 
-        this.camera.position.z = 5;
+        this.camera.position.z = 25;
 
         const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
         this.scene.add(ambientLight);
@@ -29,12 +29,38 @@ export class GeneLab {
         window.addEventListener('resize', () => this.onWindowResize());
     }
 
-    showBall() {
+    addBall(x = 0, y = 0, z = 0, color) {
         const geometry = new THREE.SphereGeometry(1, 32, 32);
-        const material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+        const material = new THREE.MeshStandardMaterial({ color: color });
         const sphere = new THREE.Mesh(geometry, material);
+        sphere.position.set(x, y, z);
         this.scene.add(sphere);
         return sphere;
+    }
+
+    addConnectedBall(parent) {
+        const child = this.addBall(parent.position.x, parent.position.y-3, parent.position.z, parent.material.color);
+
+        //parent.add(child);
+
+        //this.scene.add(child);
+
+        return child;
+
+    }
+
+    addStickBall(parent) {
+        const child = this.addBall(parent.position.x, parent.position.y-5, parent.position.z, parent.material.color);
+        //this.scene.add(child);
+
+        const material = new THREE.LineBasicMaterial({ color: 0xffffff });
+        const points = [];
+        points.push(parent.position);
+        points.push(child.position);
+        const geometry = new THREE.BufferGeometry().setFromPoints(points);
+        const line = new THREE.Line(geometry, material);
+        this.scene.add(line);
+        return child;
     }
 
     animate() {
